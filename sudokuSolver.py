f115eb558d36d0055797daccdb94f1883aa0caf1
cd3vane <devane.charles98@gmail.com>
2020-07-11 17:30:10 -0400

    board, print board function and finding empty board space has been added

---
+++ sudokuSolver.py
@@ -0,0 +1,34 @@
+sudokuBoard = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+         [6, 0, 0, 1, 9, 5, 0, 0, 0],
+         [0, 9, 8, 0, 0, 0, 0, 6, 0],
+         [8, 0, 0, 0, 6, 0, 0, 0, 3],
+         [4, 0, 0, 8, 0, 3, 0, 0, 1],
+         [7, 0, 0, 0, 2, 0, 0, 0, 6],
+         [0, 6, 0, 0, 0, 0, 2, 8, 0],
+         [0, 0, 0, 4, 1, 9, 0, 0, 5],
+         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+def solveBoard(board):
+    printBoard()
+    for i in range(len(board))
+
+def printBoard(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def findEmptySpace(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board [i][j] == 0:
+                return (i, j)
+    return 0
