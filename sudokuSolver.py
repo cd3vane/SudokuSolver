771a2fc7cd887c87ad8799c75c1cf5471644a915
cd3vane <devane.charles98@gmail.com>
2020-07-11 20:07:50 -0400

    working text solver portion of the project

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -8,14 +8,24 @@ sudokuBoard = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
          [0, 0, 0, 4, 1, 9, 0, 0, 5],
          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
+
 def solveBoard(board):
-    while checkSolved(board) == 0:
-        printBoard(board)
-        empty_space = findEmptySpace(board)
-        
+    printBoard(board)
+    print("---------------------------")
+    current_space = findEmptySpace(board)
+    if not findEmptySpace:
+        return True
+    else:
+        row, column = current_space
+    for i in range(1,10):
+        if isValidEntry(board, i, (row, column)):
+            board[row][column] = i
 
+            if solveBoard(board):
+                return True
 
-
+            board[row][column] = 0
+    return False
 
 
 def printBoard(board):
@@ -32,17 +42,38 @@ def printBoard(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
+
 def findEmptySpace(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
-            if board [i][j] == 0:
+            if board[i][j] == 0:
                 return (i, j)
     return 0
+
 
 def checkSolved(board):
     if findEmptySpace(board) == 0:
         return 1
     return 0
 
+
+def isValidEntry(board, value, position):
+    # Check Rows
+    for i in range(len(board[0])):
+        if board[position[0]][i] == value and position[1] != i:
+            return False
+    # Check Columns
+    for i in range(len(board)):
+        if board[i][position[1]] == value and position[0] != i:
+            return False
+    # Check Square
+    square_x = position[1] // 3
+    square_y = position[0] // 3
+
+    for i in range(square_y*3, square_y*3 + 3):
+        for j in range(square_x * 3, square_x*3 + 3):
+            if board[i][j] == value and (i,j) != position:
+                return False
+    return True
 
 solveBoard(sudokuBoard)
